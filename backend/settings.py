c00ddefd5688a305a1f87b19a2f035c9f3f80b93
yousuffaraz006 <yousuffaraz006@gmail.com>
2025-02-02 10:11:39 +0530

    Allowed Origins Updated

--- backend/settings.py
+++ backend/settings.py
@@ -168,6 +168,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-CORS_ALLOWED_ORIGINS = [
-    'http://localhost:5173',
-]
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOW_CREDENTIALS = True
